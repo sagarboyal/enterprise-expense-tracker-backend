b3b9ce0c77e87e50f31fe0d65b912d9e8cde2487
AditShh28 <ak2057338@gmail.com>
2025-04-08 21:39:13 +0530

    first commit

---
+++ src/test/java/com/team7/enterpriseexpensemanagementsystem/EnterpriseExpenseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team7.enterpriseexpensemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseExpenseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
