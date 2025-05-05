49cad43ddabfa3f4a06ad2d796b87df5cbb4c13d
sagarboyal <sboyal12@gmail.com>
2025-05-05 22:57:52 +0530

    feat(security): integrated Spring Security with basic HTTP and form login configuration

---
+++ src/main/java/com/team7/enterpriseexpensemanagementsystem/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.team7.enterpriseexpensemanagementsystem.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain authenticationFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().permitAll())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
