a6f51aba418a4bdf3f76a9851943495798f4a0f8
sagarboyal <sboyal12@gmail.com>
2025-05-21 06:39:42 +0530

    feat(expenses): Refactoring codes!
    -> added authUtil for repetitive @AuthenticationPrinciple.
    -> Rename Expense and Category Response to PagedResponse!
    -> Added ExpenseResponse for efficient data retrieval.
    -> new field added status and message!
    -> remove status form user!

---
+++ src/main/java/com/team7/enterpriseexpensemanagementsystem/payload/response/ExpensePagedResponse.java
@@ -0,0 +1,23 @@
+package com.team7.enterpriseexpensemanagementsystem.payload.response;
+
+import com.team7.enterpriseexpensemanagementsystem.dto.ExpenseDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ExpensePagedResponse {
+    private List<ExpenseDTO> expenses;
+    private Integer pageNumber;
+    private Integer pageSize;
+    private Long totalElements;
+    private Integer totalPages;
+    private boolean lastPage;
+}
+
