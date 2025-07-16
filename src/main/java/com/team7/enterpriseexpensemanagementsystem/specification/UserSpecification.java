d596046963c6facbf1c5a3064699dc06170253f8
sagarboyal <sboyal12@gmail.com>
2025-07-16 12:00:51 +0530

    bugs(user): user-filter bug fixed!

--- src/main/java/com/team7/enterpriseexpensemanagementsystem/specification/UserSpecification.java
+++ src/main/java/com/team7/enterpriseexpensemanagementsystem/specification/UserSpecification.java
@@ -8,17 +8,17 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class UserSpecification {
     public static Specification<User> hasName(String name) {
-        return (root, query, criteriaBuilder) -> name == null ?
+        return (root, query, criteriaBuilder) -> name == null || name.trim().isEmpty() ?
                 criteriaBuilder.conjunction() :
                 criteriaBuilder.like(criteriaBuilder.lower(root.get("fullName")), "%" + name.toLowerCase() + "%");
     }
     public static Specification<User> hasEmail(String email) {
-        return (root, query, criteriaBuilder) -> email == null ?
+        return (root, query, criteriaBuilder) -> email == null || email.trim().isEmpty() ?
                 criteriaBuilder.conjunction() :
                 criteriaBuilder.like(criteriaBuilder.lower(root.get("email")), "%" + email.toLowerCase() + "%");
     }
     public static Specification<User> hasRole(String role) {
-        return (root, query, criteriaBuilder) -> role == null ?
+        return (root, query, criteriaBuilder) -> role == null || role.trim().isEmpty() ?
                 criteriaBuilder.conjunction()
                 : criteriaBuilder.equal(root.get("roles").get("roleName"), role.toUpperCase());
     }
@@ -30,7 +30,7 @@ public class UserSpecification {
             assert query != null;
             query.groupBy(root.get("id"));
             query.having(criteriaBuilder.greaterThanOrEqualTo(criteriaBuilder.sum(expenseJoin.get("amount")), minAmount));
-            return criteriaBuilder.conjunction(); // return something; actual filtering is in groupBy/having
+            return criteriaBuilder.conjunction();
         };
     }
 
