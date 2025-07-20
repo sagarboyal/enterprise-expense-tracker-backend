deec666d4de55a8c1775e19f0ae649046eecb458
sagarboyal <sboyal12@gmail.com>
2025-07-20 14:23:02 +0530

    in cloudinary service file validation added!

--- src/main/java/com/team7/enterpriseexpensemanagementsystem/serviceImpl/CloudinaryImageServiceImpl.java
+++ src/main/java/com/team7/enterpriseexpensemanagementsystem/serviceImpl/CloudinaryImageServiceImpl.java
@@ -3,22 +3,41 @@ package com.team7.enterpriseexpensemanagementsystem.serviceImpl;
 import com.cloudinary.Cloudinary;
 import com.team7.enterpriseexpensemanagementsystem.exception.ApiException;
 import com.team7.enterpriseexpensemanagementsystem.service.CloudinaryImageService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.unit.DataSize;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Map;
 
 @Service
-@RequiredArgsConstructor
 public class CloudinaryImageServiceImpl implements CloudinaryImageService {
+
     private final Cloudinary cloudinary;
+    private final DataSize maxFileSize;
+
+    public CloudinaryImageServiceImpl(
+            Cloudinary cloudinary,
+            @Value("${cloudinary.max-file-size}") DataSize maxFileSize) {
+        this.cloudinary = cloudinary;
+        this.maxFileSize = maxFileSize;
+    }
 
     @Override
     public Map uploadImage(MultipartFile file) {
+
+        if (file.isEmpty()) {
+            throw new ApiException("File is empty! Please select a file to upload.");
+        }
+
+        if (file.getSize() > this.maxFileSize.toBytes()) {
+            throw new ApiException(String.format("File size cannot exceed %d MB.",
+                    this.maxFileSize.toMegabytes()));
+        }
+
         try {
-           return this.cloudinary.uploader().upload(file.getBytes(), Map.of());
+            return this.cloudinary.uploader().upload(file.getBytes(), Map.of("folder", "supporting docs"));
         } catch (IOException e) {
             throw new ApiException("Image upload failed! error -> " + e.getMessage());
         }
