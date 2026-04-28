926869e9356faf831eda03fd4d2244ed6d5ee49a
sagarboyal <sboyal12@gmail.com>
2026-04-28 13:24:30 +0530

    fix: postgres timeZone issue

--- src/main/java/com/main/trex/EnterpriseExpenseManagementSystemApplication.java
+++ src/main/java/com/main/trex/EnterpriseExpenseManagementSystemApplication.java
@@ -4,10 +4,13 @@ package com.main.trex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class EnterpriseExpenseManagementSystemApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(EnterpriseExpenseManagementSystemApplication.class, args);
 	}
 
